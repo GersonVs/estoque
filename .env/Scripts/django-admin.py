b7cf61143aa9edb52e127bd7160cc848f47d9cef
GersonVs <gersonssjgod@gmail.com>
2020-07-09 03:08:42 -0300

    .

--- .env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\gerso\area de trabalho\projeto 1\estoque\.env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
